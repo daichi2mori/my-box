c2866f460975f63006ab072d6d4d2232c4026aab
daichi2mori <daichi2mori@gmail.com>
2024-09-04 18:24:34 +0900

    fix: url encoding and image loading to eager

--- frontend/app/components/book.tsx
+++ frontend/app/components/book.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Book = ({ path }: { path: string }) => {
+	const encodedPath = encodeURIComponent(path);
+
 	return (
 		<Link href={`/ero/detail/${path}`}>
 			<Image
-				src={`http://localhost:3005/cover/${path}`}
+				src={`http://localhost:3005/cover/${encodedPath}`}
 				width={300}
 				height={300}
 				priority
+				loading="eager"
 				alt="i"
 				className="w-full h-full object-contain"
 			/>
