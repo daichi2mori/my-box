f00a5442c16e23a141bf45b14edba72ebbb34461
daichi2mori <daichi2mori@gmail.com>
2024-10-20 15:42:42 +0900

    add: allow credentials

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"backend/ero"
 	"backend/manga"
 	"backend/utils"
+	"backend/video"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -14,12 +15,14 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3004, http://192.168.11.9:3004, https://my-box.daichi2mori.com",
-		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowOrigins:     "http://localhost:3004, http://192.168.11.9:3004, https://my-box.daichi2mori.com",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowCredentials: true,
 	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World")
 	})
+
 	app.Get("/ero/page/:page", ero.Page)
 	app.Get("/ero/cover/:path", ero.Cover)
 	app.Get("/ero/author/:path", ero.Author)
@@ -33,6 +36,8 @@ func main() {
 	app.Get("/manga/book/:path", manga.Book)
 	app.Get("/manga/total-page", manga.TotalPage)
 	app.Get("/manga/image/:path", manga.Image)
+
+	app.Get("/video/:path", video.Video)
 
 	// TSLの残骸
 	// cert := utils.GetTLS()
